ad85e597c5322a8f69d4c0bb06d13adbc0435763
atokolyi <alex.tokolyi@gmail.com>
2025-07-14 12:32:50 -0400

    renamed pkg

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='TheSpliceGirls',
+setup(name='leafidex',
       version='0.1',
       description='An annotation pipeline for transcript splicing events',
-      url='https://github.com/atokolyi/TheSpliceGirls',
+      url='https://github.com/atokolyi/leafidex',
       author='Alex Tokolyi',
       author_email='alex.tokolyi@gmail.com',
       license='gplv3',
-      packages=['TheSpliceGirls'],
+      packages=['leafidex'],
       install_requires=[
           'pandas','pyranges1','requests','numpy'
       ],
